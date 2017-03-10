51c743a9ff3fa475daaed7b49094d2d7fd12fa60
S2R3 <S2R3@users.noreply.github.com>
2017-03-09 23:59:12 -0500

    Add files via upload

--- Venue.java
+++ Venue.java
@@ -9,28 +9,32 @@ import javafx.scene.image.ImageView;
 import javax.swing.*;
 
 public class Venue {
-    private Car[] cars;
+//    private Car[] cars;
     private CheckPoint[] checkPoints;
-    Image car1;
-    ImageView imageView1;
+//    Image car1;
+//    ImageView imageView1;
 
 
     public Venue(){
-        //cars = new Car[4];
+//        cars = new Car[4];
         checkPoints = new CheckPoint[4];
-//        car1 = new Image("car1.png");
-        //imageView1 = new ImageView();
-       // imageView1.setImage(car1);
+//        car1 = new Image("blackcar.png");
+//        imageView1 = new ImageView();
+//        imageView1.setImage(car1);
 
 
 
-        checkPoints[0] = new CheckPoint( 100, 100, 316, 144, "A");
-        checkPoints[1] = new CheckPoint( 500, 200, 527, 287, "B");
-        checkPoints[2] = new CheckPoint( 500, 500, 345, 495, "C");
-        checkPoints[3] = new CheckPoint( 75, 500, 100, 280, "D");
+        checkPoints[0] = new CheckPoint( 0, 0, 316, 4, "A");
+        checkPoints[1] = new CheckPoint( 1000, 100, 787, 347, "B");
+        checkPoints[2] = new CheckPoint( 500, 480, 345, 450, "C");
+        checkPoints[3] = new CheckPoint( 10, 500, 10, 200, "D");
     }
-
-
+    public double getCkPointX(int value){
+        return checkPoints[value].getX();
+    }
+    public double getCkPointY(int value){
+        return checkPoints[value].getY();
+    }
 
     public CheckPoint getCheckPoints(int value){
         return checkPoints[value];
@@ -40,3 +44,4 @@ public class Venue {
 
 
 }
+
